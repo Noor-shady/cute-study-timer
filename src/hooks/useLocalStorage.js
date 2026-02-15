998eb47ad52063040dcb561fcbc6401fc1721eff
noor <c3074048@newcastle.ac.uk>
2026-02-15 20:59:10 +0000

    state from localStorage with fallback

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,3 +2,10 @@ import { useState } from 'react';
 
 export default function useLocalStorage(key, initialValue) {
   // Lazy Initialization: I use a function inside useState
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      // Look in the browser's memory for this specific key
+      const item = window.localStorage.getItem(key);
