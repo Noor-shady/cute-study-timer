c811d9fa63f7e2222e3b9074e5dd47fb90770010
noor <c3074048@newcastle.ac.uk>
2026-02-15 21:01:57 +0000

    read errors and return initial value

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -11,3 +11,7 @@ export default function useLocalStorage(key, initialValue) {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
+      console.warn(`Error reading localStorage key "${key}":`, error);
+      return initialValue;
+    }
+  });
